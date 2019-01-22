43df7e8038b8530547adf07f70a67e8bde180cd4
Julien EHLES <adai101@sysinfo.ccicampus.fr>
2019-01-22 16:42:14 +0100

    ajout classes

---
+++ Projet2_CCI/Projet2_CCI/Connexion.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projet2_CCI
+{
+    /// <summary>
+    /// Logique d'interaction pour Connexion.xaml
+    /// </summary>
+    public partial class Connexion : Window
+    {
+
+        public Connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Connexion_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindow = new MainWindow();
+            this.Hide();
+            MainWindow.Show();
+
+        }
+    }
+}
---
+++ Projet2_CCI/Projet2_CCI/Personne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet2_CCI
+{
+    public class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        // CONSTRUCTEUR
+        public Personne(string nom, string prenom)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+        }
+
+    }
+
+    public class Employe : Personne
+    {
+    }
+
+    class Client : Personne
+    {
+    }
+}
