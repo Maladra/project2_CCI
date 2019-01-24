51dfbfcfb0109526c800ce766c6b53a4795bb0dc
Julien EHLES <adai101@sysinf.pole-formation.fr>
2019-01-24 14:58:01 +0100

    modif classes

--- Projet2_CCI/Projet2_CCI/Location.cs
+++ Projet2_CCI/Projet2_CCI/Location.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace Projet2_CCI
 {
-    class Location // nom et prenom client, type de paiement(cb cheque espece), 
-                  // prix, nombre planche, type planche, marque planche, id planche ??
+    class Location // client, type de paiement(cb cheque espece), prix,
     {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string TypePaiement { get; set; }
+        public int Quantite { get; set; }
+        public decimal Prix { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        // CONSTRUCTEUR
+        public Location(string nom, string prenom, string typePaiement, int quantite, decimal prix, DateTime dateDebut, DateTime dateFin)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.TypePaiement = typePaiement;
+            this.Quantite = quantite;
+            this.Prix = prix;
+            this.DateDebut = dateDebut;
+            this.DateFin = dateFin;
+        }
+
     }
 }
--- Projet2_CCI/Projet2_CCI/Snowboard.cs
+++ Projet2_CCI/Projet2_CCI/Snowboard.cs
@@ -12,14 +12,16 @@ namespace Projet2_CCI
         public string Genre { get; set; } // sexe homme femme enfant
         public string Niveau { get; set; } // debutant intermediaire expert
         public string Style { get; set; } // Freestyle Park, freeride, carving, polyvalent
+        public decimal Prix { get; set; }
 
         // CONSTRUCTEUR
-        public Snowboard(string marque, string genre, string niveau, string style)
+        public Snowboard(string marque, string genre, string niveau, string style, decimal prix)
         {
             this.Marque = marque;
             this.Genre = genre;
             this.Niveau = niveau;
             this.Style = style;
+            this.Prix = prix;
         }
     }
 }
