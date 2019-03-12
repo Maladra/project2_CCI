0f9127c05caa56f139cf365611f4defe0419a477
Julien EHLES <adai101@sysinfo.ccicampus.fr>
2019-03-12 16:25:37 +0100

    modif DB ajout dollar

--- Projet2_CCI/Projet2_CCI/Snowboard.cs
+++ Projet2_CCI/Projet2_CCI/Snowboard.cs
@@ -8,7 +8,7 @@ namespace Projet2_CCI
 {
     class Snowboard
     {
-        public string Nom { get; set; }
+        public string Nom { get; set; } // nom du modele
         public string Marque { get; set; } // marque du snow
         public string Genre { get; set; } // sexe homme femme enfant
         public string Niveau { get; set; } // debutant intermediaire expert
